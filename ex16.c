bbebb5d7ad80b4908c13735f09277c952d47395d
Edward Dal Santo <ejds001@gmail.com>
2018-09-07 18:41:56 -0700

    finish 16

--- ex16.c
+++ ex16.c
@@ -82,3 +82,68 @@ int main(int argc, char *argv[]) {
 
   return 0;
 }
+
+// Without malloc and pointers
+
+// factory function for creating a person struct
+struct Person Person_create2(char *name, int age, int height, int weight) {
+  // find size of person in bytes and allocating memory for it in RAM, then point the pointer who at that memory location
+  struct Person who;
+  // evaluate whether expression is true / false
+  //   if true, does nothing
+  //   if false, displays an error on stderr and aborts program execution
+  // deference struct pointer and assign attributes -> is shorthand for (*sp)
+  // duplicate name and assign to name attribute of who
+  who.name = strdup(name);
+  who.age = age;
+  who.height = height;
+  who.weight = weight;
+
+  // return pointer to new person struct
+  return who;
+}
+
+void Person_destroy2(struct Person who) {
+  // evaluate whether expression is true / false
+  //   if true, does nothing
+  //   if false, displays an error on stderr and aborts program execution
+  // free up memory that OS allocated for us
+  free(who.name);
+}
+
+void Person_print2(struct Person who) {
+  printf("Name: %s\n", who.name);
+  printf("\tAge: %d\n", who.age);
+  printf("\tHeight: %d\n", who.height);
+  printf("\tWeight: %d\n", who.weight);
+}
+
+int main2(int argc, char *argv[]) {
+  // make 2 people
+  struct Person joe = Person_create2("Joe Alex", 32, 64, 140);
+  struct Person frank = Person_create2("Frank Blank", 20, 72, 180);
+
+  // print them out and where in memory
+  printf("Joe is at memory location: %p\n", &joe);
+  Person_print2(joe);
+
+  printf("Frank is at memory location: %p\n", &frank);
+  Person_print2(frank);
+
+  // make everyone age 20 years and print again
+  // again, deferencing struct point and updating field
+  joe.age += 20;
+  joe.height -= 2;
+  joe.weight += 40;
+  Person_print2(joe);
+
+  frank.age += 20;
+  frank.weight += 20;
+  Person_print2(frank);
+
+  // clean up
+  Person_destroy2(joe);
+  Person_destroy2(frank);
+
+  return 0;
+}
