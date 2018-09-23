ffc92aed2af49e87152dcb2dce5c21036d46af2d
Edward Dal Santo <ejds001@gmail.com>
2018-09-23 07:34:19 -0700

    glob

--- ex26/src/main.c
+++ ex26/src/main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <glob.h>
 
 #include "../../includes/dbg.h"
 
 #define BUFFER_SIZE 1024
 #define FLAG_INITIATOR '-'
 
-// grep for words only as log files
-// example: logfind foo bar -> looks for any log files with the words foo *and* bar and prints them out
-// optional argument -o which will transform parameters to *or* logic
-// allowed log files -> loaded from ~/.logfind
-//   can be anything the glob function allows -> start with flat list of exact files then add glob
-// output matches as scan
+/* Grep-like utility
+ * example: logfind foo bar -> looks for any log files with the words foo *and* bar and prints them out
+ * optional argument -o which will transform parameters to *or* logic
+ * */
 
-// helpers
+/* helpers */
+
 int strings_equal(const char *str1, const char *str2) {
   return strcmp(str1, str2) == 0;
 }
@@ -46,42 +46,48 @@ int main(int argc, char *argv[]) {
       num_search_terms += 1;
     }
   }
+
   // find all log files utilizing glob
-  // for each log file
-  //   open file
-  //   read each line
-
-  // relative to the current working directory when the executable is run
-  stream = fopen("./logfile.log", "r");
-  more_lines_to_read = getline(&line, &nread, stream) != -1;
-  while (more_lines_to_read) {
-    // reset match statuses to false
-    matching_search_terms = 0;
-
-    // search through all search tokens for match
-    for (i = 1; i < argc; i++) {
-      if (line_is_flag(argv[i])) continue;
-
-      // see if current search term is match
-      search_term = argv[i];
-      if (substr_match(line, search_term)) {
-        // mark token as matched if match
-        matching_search_terms += 1;
-      }
-    }
-
-    // print out match
-    if (!or_flag && (matching_search_terms == num_search_terms)) {
-      // num_search_termsprint out line if all tokens matched with AND
-      debug("%s", line);
-    } else if (or_flag && matching_search_terms) {
-      // print out line if any tokens matched with OR
-      debug("%s", line);
-    }
-
-    // read next line
+  glob_t glob_buf;
+  glob_buf.gl_offs = 0;
+  glob("*.log", GLOB_DOOFFS, NULL, &glob_buf);
+  debug("matched files %d", (int)glob_buf.gl_pathc);
+  char **log_files = glob_buf.gl_pathv;
+  // check each log file for match
+  for (i = 0; i < glob_buf.gl_pathc; i++) {
+    // relative to the current working directory when the executable is run
+    stream = fopen(log_files[i], "r");
     more_lines_to_read = getline(&line, &nread, stream) != -1;
+    while (more_lines_to_read) {
+      // reset match statuses to false
+      matching_search_terms = 0;
+
+      // search through all search tokens for match
+      for (i = 1; i < argc; i++) {
+        if (line_is_flag(argv[i])) continue;
+
+        // see if current search term is match
+        search_term = argv[i];
+        if (substr_match(line, search_term)) {
+          // mark token as matched if match
+          matching_search_terms += 1;
+        }
+      }
+
+      // print out match
+      if (!or_flag && (matching_search_terms == num_search_terms)) {
+        // num_search_termsprint out line if all tokens matched with AND
+        debug("%s", line);
+      } else if (or_flag && matching_search_terms) {
+        // print out line if any tokens matched with OR
+        debug("%s", line);
+      }
+
+      // read next line
+      more_lines_to_read = getline(&line, &nread, stream) != -1;
+    }
   }
+
   return 0;
 error:
   return -1;
