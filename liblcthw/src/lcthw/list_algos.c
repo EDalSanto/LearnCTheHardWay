c0e1afcf3742f08f0e5947eadf8b244995856b5d
Edward Dal Santo <ejds001@gmail.com>
2018-10-08 08:26:54 -0700

    get tests to run

---
+++ liblcthw/src/lcthw/list_algos.c
@@ -0,0 +1,13 @@
+#include "list.h"
+#include "list_algos.h"
+
+int List_bubble_sort(List *list, List_compare cmp_cb) {
+
+  return 1;
+}
+
+List *List_merge_sort(List *list, List_compare cmp_cb) {
+  List *res = List_create();
+
+  return res;
+}
---
+++ liblcthw/src/lcthw/list_algos.h
@@ -0,0 +1,14 @@
+#ifndef lcthw_List_algos_h
+#define lcthw_List_algos_h
+
+#include <stdlib.h>
+#include <lcthw/list.h>
+
+// create a type that is a pointer to a function named List_compare that takes two strings as args
+// returns an integer
+typedef int (*List_compare) (char *value1, char *value2);
+
+int List_bubble_sort(List *list, List_compare cmp);
+List *List_merge_sort(List *list, List_compare cmp);
+
+#endif
